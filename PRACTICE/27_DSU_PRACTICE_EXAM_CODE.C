1faa265d429d696e3f9ae74dad85981474d3f318
Sahil Zalte <zaltesahil94@gmail.com>
2024-11-26 23:41:10 +0530

    .

--- PRACTICE/27_DSU_PRACTICE_EXAM_CODE.C
+++ PRACTICE/27_DSU_PRACTICE_EXAM_CODE.C
@@ -1,8 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 5
-int top = -1, stack[MAX];
-void push()
+#define max 5
+int top = -1, stack[max];
+
+void PUSH()
 {
-    
+    int value;
+    if (top == max - 1)
+    {
+        printf("Stack is Overflow!");
+    }
+    else
+    {
+        printf("Enter element to PUSH:");
+        scanf("%d", &value);
+        top++;
+        stack[top] = value;
+    }
+}
+
+void POP()
+{
+    if (top == -1)
+    {
+        printf("Stack is Underflow !");
+    }
+    else
+    {
+        printf("Deleted element is : %d", stack[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    int i;
+    if (top == -1)
+    {
+        printf("Stack is empty !");
+    }
+    else
+    {
+        for (i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+int main()
+{
+    int ch;
+    setbuf(stdout, NULL);
+    printf("***Stack Menu ***\n");
+    printf("\n1.PUSH \n2.POP \n3.DISPLAY \n4.EXIT");
+    while (1)
+    {
+        printf("enter user choise:");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            PUSH();
+            break;
+        case 2:
+            POP();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+            break;
+
+        default:
+            printf("Wrong Choise");
+            break;
+        }
+    }
+    return 0;
 }
